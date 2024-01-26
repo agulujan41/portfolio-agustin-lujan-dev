a9f4285fd733a2e48228dca2727d397a9a01299d
agulujan41 <agulujan41proj@gmail.com>
2024-01-26 17:33:29 -0300

    adding splash 6

--- src/components/SplashLoader/scriptLoading.js
+++ src/components/SplashLoader/scriptLoading.js
@@ -1,15 +1,15 @@
-document.addEventListener('readystatechange', function(e) {
-    if(document.readyState === "loading"){
-     console.log("content is loading")
-    }
-    else if(document.readyState === "interactive"){ 
-     console.log("dom content loaded but resources like image or script has not been loaded yet.")
-    }
-    else{
-        let loader = document.getElementById("loader");
-        setTimeout(() => {
-          
-          loader.classList.add("close");
-        }, 4000);
-    }
-   });
+document.addEventListener("readystatechange", function (e) {
+  if (document.readyState === "loading") {
+    console.log("content is loading");
+  } else if (document.readyState === "interactive") {
+    console.log(
+      "dom content loaded but resources like image or script has not been loaded yet."
+    );
+  } else {
+    console.log("loaded");
+    let loader = document.getElementById("loader");
+    setTimeout(() => {
+      loader.classList.add("close");
+    }, 4000);
+  }
+});
