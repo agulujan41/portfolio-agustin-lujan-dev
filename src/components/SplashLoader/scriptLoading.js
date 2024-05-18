4b2e7b1afe2ee46ea149f577da144bccc9eaa22e
agulujan41 <agulujan41proj@gmail.com>
2024-05-18 16:54:10 -0300

    loader fixing 1

--- src/components/SplashLoader/scriptLoading.js
+++ src/components/SplashLoader/scriptLoading.js
@@ -1,26 +1,10 @@
-document.addEventListener("readystatechange", function (e) {
-  if (document.readyState === "loading") {
-    console.log("content is loading");
-  } else if (document.readyState === "interactive") {
-    console.log(
-      "dom content loaded but resources like image or script has not been loaded yet."
-    );
-  } else {
-    console.log("loaded");
-    let loader = document.getElementById("loader");
-    setTimeout(() => {
-      loader.classList.add("close");
-    }, 3000);
-  }
-});
 
 if(document.readyState === "complete") {
   // Fully loaded!
   console.log("loaded");
   let loader = document.getElementById("loader");
-  setTimeout(() => {
-    loader.classList.add("close");
-  }, 3000);
+  loader.classList.remove("show");
+  loader.classList.add("close");
 }
 else if(document.readyState === "interactive") {
   // DOM ready! Images, frames, and other subresources are still downloading.
@@ -37,9 +21,7 @@ else {
       // Fully loaded!
       console.log("loaded");
       let loader = document.getElementById("loader");
-      setTimeout(() => {
-        loader.classList.add("close");
-      }, 3000);
+      loader.classList.remove("show");
       loader.classList.add("close");
     });
     
